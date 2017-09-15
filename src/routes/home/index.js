b89ff80bcd5b6a6237c6ca1417895ee40096fa36
Matt Loria <loria.matt@gmail.com>
2017-09-15 18:07:02 -0400

    Eslint fixes. Error testing

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -2,13 +2,9 @@ import { h, Component } from 'preact';
 import style from './style';
 
 export default class Home extends Component {
-
-	state = {
-		response: ''
-	};
-
+	
 	registerDummyUser = () => {
-		const testEmail = 'aNewTest2@test.com';
+		const testEmail = 'test1@yahoo.com';
 		const testPassword = 'testpassword99';
 		const testPasswordConfirm = 'testpassword99';
 		let registrationPackage = {
@@ -16,25 +12,33 @@ export default class Home extends Component {
 			email: testEmail,
 			password: testPassword,
 			password_confirmation: testPasswordConfirm,
-			confirm_success_url: 'http://localhost:3000',
+			confirm_success_url: 'http://localhost:8080'
 		};
-
 
 		fetch('http://localhost:3000/auth', {
 		  method: 'POST',
 		  headers: {
-		    'Accept': 'application/json',
+		    Accept: 'application/json',
 		    'Content-Type': 'application/json'
 		  },
-		  body: JSON.stringify(registrationPackage)});
+		  body: JSON.stringify(registrationPackage) })
+			.then( (resp) => resp.json())
+			.then( (data) => this.setState( { response: data.errors.full_messages[0] } ));
+	}
+
+	constructor(){
+		super();
+		this.state = {
+			response: ''
+		};
 	}
 
 	render() {
 		return (
 			<div class={style.home}>
-				<h1>Home</h1>
-				<p>This is the Home component.</p>
-				<button onClick={ this.registerDummyUser }>Register dummy user</button>
+				<h1>Register a new User</h1>
+				<button onClick={this.registerDummyUser}>Register dummy user</button>
+				<div>Response: {this.state.response}</div>
 			</div>
 		);
 	}
