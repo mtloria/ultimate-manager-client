ed754c28dc090731c61101089c4b1a955ef6537e
Matt Loria <loria.matt@gmail.com>
2017-09-16 23:53:02 -0400

    Added input fields for registration

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -3,15 +3,13 @@ import style from './style';
 
 export default class Home extends Component {
 	
-	registerDummyUser = () => {
-		const testEmail = 'test1@yahoo.com';
-		const testPassword = 'testpassword99';
-		const testPasswordConfirm = 'testpassword99';
+	registerUser = () => {
 		let registrationPackage = {
-			config_name: 'default',
-			email: testEmail,
-			password: testPassword,
-			password_confirmation: testPasswordConfirm,
+			email: this.state.email,
+			password: this.state.password,
+			password_confirmation: this.state.passwordConfirm,
+			name: this.state.name,
+			team_name: this.state.teamName,
 			confirm_success_url: 'http://localhost:8080'
 		};
 
@@ -21,15 +19,40 @@ export default class Home extends Component {
 		    Accept: 'application/json',
 		    'Content-Type': 'application/json'
 		  },
-		  body: JSON.stringify(registrationPackage) })
-			.then( (resp) => resp.json())
-			.then( (data) => this.setState( { response: data.errors.full_messages[0] } ));
+		  body: JSON.stringify(registrationPackage) });
+	}
+
+	setName = (e) => {
+		this.setState( { name: e.target.value } );
+	}
+
+	setTeamName = (e) => {
+		this.setState( { teamName: e.target.value } );
+	}
+
+	setEmail = (e) => {
+		// TODO move into component that checks for valid email format
+		this.setState( { email: e.target.value } );
+	}
+
+	setPassword = (e) => {
+		// TODO move into component that checks for valid password requirements
+		this.setState( { password: e.target.value } );
+	}
+
+	setPasswordConfirm = (e) => {
+		// TODO move into component from the above 'TODO'. Also, check that passwords match
+		this.setState( { passwordConfirm: e.target.value } );
 	}
 
 	constructor(){
 		super();
 		this.state = {
-			response: ''
+			name: '',
+			teamName: '',
+			email: '',
+			password: '',
+			passwordConfirm: ''
 		};
 	}
 
@@ -37,8 +60,12 @@ export default class Home extends Component {
 		return (
 			<div class={style.home}>
 				<h1>Register a new User</h1>
-				<button onClick={this.registerDummyUser}>Register dummy user</button>
-				<div>Response: {this.state.response}</div>
+				<input type="text" placeholder="Name" onBlur={this.setName} />
+				<input type="text" placeholder="Team Name" onBlur={this.setTeamName} />
+				<input type="text" placeholder="Email" onBlur={this.setEmail}>Email</input>
+				<input type="text" placeholder="Password" onBlur={this.setPassword}>Password</input>
+				<input type="text" placeholder="Confirm Password" onBlur={this.setPasswordConfirm}>Confirm Password</input>
+				<button onClick={this.registerUser}>Register</button>
 			</div>
 		);
 	}
